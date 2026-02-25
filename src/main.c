7a25e5ce0a02c4c9be893577bd01c1107c3ae427
rasamad <rasamad@rasamad.myguest.virtualbox.org>
2026-02-25 13:18:10 +0100

    parsing done

--- src/main.c
+++ src/main.c
@@ -1,17 +1,53 @@
-#include <stdio.h>
 
-int parse_arg(int ac, char **av)
+#include <stdio.h>  //printf()
+#include <string.h> //strcmp()
+
+typedef struct s_data{
+    char *host;
+    char *host_two;
+}t_data;
+
+void print_help()
+{
+    printf("Usage: ft_traceroute [--help] <destination>\n");
+    printf("Trace route to an IPv4 host (address or hostname).\n\n");
+    printf("Options:\n");
+    printf("  --help    Display this help and exit\n");
+}
+
+int parse_arg(int ac, char **av, t_data *data)
 {
     if (ac <= 1)
-        return(printf("missing destination %s\n", av[0]), 1);
+        return(printf("missing destination\n"), 1);
+    for(int i = 1 ; i < ac ; i++)
+    {
+        if (av[i][0] == '-')
+        {
+            if (strcmp(av[i], "--help") == 0)
+                return(print_help(), 0);
+            else
+                return(printf("Bad option `%s' (argc %d)\n", av[i], i), 1);
+        }
+        if (av[i] && data->host == NULL)
+           data->host = av[i];
+        else if (av[i] && data->host_two == NULL)
+            data->host_two = av[i];
+        else if (av[i] && data->host_two != NULL)
+            return(printf("Extra arg `%s' (argc %d)\n", av[i], i), 1);
+    }
+
     return 0;
 }
 
 int main(int ac, char **av)
 {
-    parse_arg(ac, av);
+    t_data data;
+    data.host = NULL;
+    data.host_two = NULL;
+    if (parse_arg(ac, av, &data))
+        return 2;
+    printf("|%s|\n", data.host);
 
-    printf("Go traceroute =D\n");
-
+    
     return 0;
 }
