5c3e1dcadced8b66c7a477ea2b8f849e7421c848
rasamad <rasamad@rasamad.myguest.virtualbox.org>
2026-03-23 13:35:52 +0100

    -m max hops

--- src/parsing.c
+++ src/parsing.c
@@ -1,26 +1,60 @@
 #include "ft_traceroute.h"
 
-/* --- AIDE ET PARSING --- */
-void print_help() {
-    printf("Usage: ft_traceroute [--help] <destination>\n");
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	long	n;
+
+	n = 0;
+	sign = 1;
+	while ((*str == ' ') || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		if (n < 0 && sign == 1)
+			return (-1);
+		if (n < 0 && sign == -1)
+			return (0);
+		n *= 10;
+		n = n + *str - '0';
+		str++;
+	}
+    if (*str != '\0'){
+        return(-1);
+    }
+	return (n * sign);
 }
 
 int parse_arg(int ac, char **av, t_data *data) {
     if (ac <= 1) 
-        return (fprintf(stderr, "Specify \"host\" missing\n"), 1);
+        return(printf("Usage: ft_traceroute [--help] <destination>\n"), 1);
     for (int i = 1; i < ac; i++) 
     {
-        if (strcmp(av[i], "--help") == 0) { print_help(); exit(0); }
-        if (strcmp(av[i], "-m") == 0) { 
-            if(av[i+1]){
-                int res = atoi(av[i+1]);
-                if(res <= 0)
-                    printf("first hop out of range\n");
-                printf("|%s|   res = |%d|\n", av[i+1], res);
+        if (strcmp(av[i], "--help") == 0)
+            return(printf("Usage: ft_traceroute [--help] <destination>\n"), 1);
+        if (strcmp(av[i], "-m") == 0) 
+        {
+            if(av[++i])
+            {
+                int hops = ft_atoi(av[i]);
+                if (hops <= 0)
+                    return (printf("first hop out of range\n"), 1);
+                printf("arg = |%s|   res = |%d|\n", av[i], hops);
             }
+            else
+                return (fprintf(stderr, "Option `-m' requires an argument: `-m max_ttl'\n"), 2);
+            continue;
         }
-        if (av[i][0] == '-') return (fprintf(stderr, "Bad option `%s'\n", av[i]), 1);
-        if (!data->host) data->host = av[i];
+        if (av[i] && av[i][0] == '-')
+            return (fprintf(stderr, "Bad option `%s'\n", av[i]), 1);
+        if (!data->host)
+            data->host = av[i];
         else
             return (fprintf(stderr, "ft_traceroute: extra operand `%s'\n", av[i]), 1);
     }
